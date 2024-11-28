7e5a4f937fa4a2cd6c885161394d69c8fb32b6e7
ElenaGM <egutmor699@g.educaand.es>
2024-11-28 20:31:42 +0100

    aaaaa

---
+++ src/hola/Prueba3.java
@@ -0,0 +1,13 @@
+package hola;
+
+import java.util.Scanner;
+public class Prueba3 {
+	public static void main(String[] args) {
+Scanner scanner = new Scanner(System.in);
+
+System.out.println("Introduce un número");
+int numero = scanner.nextInt();
+
+
+}
+}
