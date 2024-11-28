9397c72da6689225e19f3b6360ce78aaeac01bda
ElenaGM <egutmor699@g.educaand.es>
2024-11-28 20:36:19 +0100

    aaaa

--- src/hola/Prueba3.java
+++ src/hola/Prueba3.java
@@ -8,6 +8,12 @@ Scanner scanner = new Scanner(System.in);
 System.out.println("Introduce un número");
 int numero = scanner.nextInt();
 
+if (numero % 2 <= 0) && (numero % 3 <= 0) {
 
+}if else(numero % 2 <= 0) {
+	System.out.println("El número "+ numero + " es múltiplo de 2");
+	
+	
+}
 }
 }
