6a68e7206beeb2d9c574c4c3941c9422504f75cb
ElenaGM <egutmor699@g.educaand.es>
2024-12-05 19:33:04 +0100

    ole

--- src/hola/Prueba3.java
+++ src/hola/Prueba3.java
@@ -8,12 +8,17 @@ Scanner scanner = new Scanner(System.in);
 System.out.println("Introduce un número");
 int numero = scanner.nextInt();
 
-if (numero % 2 <= 0) && (numero % 3 <= 0) {
-
-}if else(numero % 2 <= 0) {
+if (numero % 2 == 0 && numero % 3 == 0) {
+	System.out.println("El número "+ numero + " es múltiplo de 2 y de 3");
+} if (numero % 3 == 0 && numero % 2 != 0) {
+	System.out.println("El número "+ numero + " es múltiplo de 3");
+} if (numero % 2 == 0 && numero % 3 != 0){
 	System.out.println("El número "+ numero + " es múltiplo de 2");
+} if (numero % 2 != 0 && numero % 3 != 0){
+	System.out.println("El número "+ numero + " NO es múltiplo ni de 2 ni de 3");
+}
 	
-	
+scanner.close();
 }
 }
-}
+
