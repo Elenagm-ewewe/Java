2aec57b3d8d7022460c7b1877c57af6b29cbe515
Elenagm-ewewe <egutmor699@g.educaand.es>
2025-01-21 00:39:57 +0100

    aa

--- src/Rel2/Ejercicio7.java
+++ src/Rel2/Ejercicio7.java
@@ -9,21 +9,30 @@ Scanner scanner = new Scanner(System.in);
 
 System.out.println("¿Cuántos números vas a introducir?");	
 
-int cant = scanner.nextInt();
+double cant = scanner.nextDouble();
 long num =0;
 long sum = 0;
 
-for (int i=1; i<cant + 1; i++ ) {
-	System.out.println("Numero" + i);
-	num = scanner.nextLong();
-	sum = num + sum;
+if (cant<0 || cant==0 || cant%1!=0) {
+do {
+	System.out.println("Por favor, introduce un numero entero POSITIVO");
+	cant = scanner.nextDouble();
+}while (cant<=0 || cant%1!=0);
 }
-	
-float media = sum/cant;
 
-System.out.println("La media de los numeros introducidos es " + media);
 
-scanner.close();
 
+
+	for (int i=1; i<cant + 1; i++ ) {
+		System.out.println("Numero " + i);
+		num = scanner.nextLong();
+		sum = num + sum;
+	}
+		
+	double media = sum/cant;
+
+	System.out.println("La media de los numeros introducidos es " + media);
+
+	scanner.close();
 }
 }
---
+++ src/Rel2/Ejercicio8.java
@@ -0,0 +1,58 @@
+package Rel2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Ejercicio8 {
+       public static void main(String[]args) {
+       Scanner scanner = new Scanner(System.in);
+       ArrayList<Integer> numeros = new ArrayList<>();
+	
+int numero ;
+boolean continuar = true;
+int veces = 1;
+String SN;
+
+do {
+   veces=veces+1;
+   System.out.println("Introduce un número");
+   numero = scanner.nextInt();
+
+   System.out.println("¿Desea continuar? (S/N)");
+		SN = scanner.next().toUpperCase();
+   
+   
+if (SN!="S" && SN!="N") {	
+
+	  System.out.println("No vale, pon otro");
+		SN = scanner.next().toUpperCase();
+
+}else {
+	switch (SN) {
+	  case "S": 
+		  continuar = true;
+	  break;
+	  
+	  case "N": 
+		  continuar = false;
+	  break;
+	  }
+
+  }
+  
+numeros.add(numero);
+   
+
+} while (continuar == true) ;
+	
+
+//escribir el vector
+
+
+for (int num : numeros) {
+	System.out.print(num+ ", ");
+}
+
+
+scanner.close();
+}
+}
